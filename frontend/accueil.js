7cbfcc6fed6070ce5f5e28d0efa25efd5f7e62d2
Merzak Benaissi <merzak89merzak@gmail.com>
2021-05-06 00:53:01 +0200

    JS page d'accueil, apparition des produits

---
+++ frontend/accueil.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const getTeddies = () => {
+    return fetch('http://localhost:3000/api/teddies')
+    .then((response) =>  response.json())
+    .then((products) => products)
+    .catch(function(error) {
+        alert(error)
+    })
+}
+
+(async  () => {
+    const teddies = await getTeddies()
+    for(const teddy of teddies){
+        displayTeddies(teddy)
+    }
+}) ()
+
+const displayTeddies = (ted) => {
+    const templateElement = document.querySelector('#templateTeddies')
+    const cloneElement = document.importNode(templateElement.content, true)
+   
+    cloneElement.querySelector('.card-title').textContent = ted.name
+    cloneElement.querySelector('.card-img-top').src = ted.imageUrl
+    cloneElement.querySelector('.price').textContent = `${ted.price/100},00 €`
+    cloneElement.querySelector('.url').href= `product.html?id=${ted._id}`
+
+    document.querySelector('.main').appendChild(cloneElement)
+}
+
+//cette fonction récupère le nombre de produits stockés dans le localStorage quand on raffraichi notre page
+let onloadCartNumbers = () => {
+    let productNumbers = localStorage.getItem('cartNumbers');
+    if (productNumbers) {
+        document.querySelector('.cartIcone').textContent = productNumbers
+        
+    }
+}
+onloadCartNumbers();
